478b01ca65b478d86e8922a4efcd87464f7792d5
henry-e-n <59893778+henry-e-n@users.noreply.github.com>
2022-08-09 15:39:31 -0400

    name lgndbdt

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='legend_psd_bdt',
+    name='lgndbdt',
     version='0.0.1',
     author='Henry Nachman',
     author_email='henachman@unc.edu',
     description='Python package for extracting PSDs and training a BDT',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/henry-e-n/legend_psd_bdt',
+    url='https://github.com/henry-e-n/lgndbdt',
     license='Apache-2.0',
     packages=['ML_utils', 'raw_to_bdt', 'extraction_utils'],
     install_requires=['h5py>=3.2.0',
