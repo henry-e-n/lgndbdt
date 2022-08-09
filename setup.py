45d3f8bb6e54f5833394b0c83313f7141240f89e
henry-e-n <nelliot@live.unc.edu>
2022-08-09 14:24:50 -0400

    new setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,28 @@
-import setuptools_scm 
-from setuptools import setup
+import setuptools
 
-setup()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='legend_psd_bdt',
+    version='0.0.1',
+    author='Henry Nachman',
+    author_email='henachman@unc.edu',
+    description='Python package for extracting PSDs and training a BDT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/henry-e-n/legend_psd_bdt',
+    license='Apache-2.0',
+    packages=['ML_utils', 'raw_to_bdt', 'extraction_utils'],
+    install_requires=['h5py>=3.2.0',
+                        'iminuit',
+                        'matplotlib',
+                        'numpy>=1.21',
+                        'pandas',
+                        'parse',
+                        'pint',
+                        'pyfftw',
+                        'scipy',
+                        'tqdm',
+                        'lightgbm']
+)
