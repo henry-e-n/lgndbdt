db15da751103fd92092f5b38bb25401bb824a372
henry-e-n <nelliot@live.unc.edu>
2022-08-18 13:47:33 -0400

    beginning Bivariate PCA code

---
+++ lgndbdt/ML_utils/MultiVarCorr.py
@@ -0,0 +1,5 @@
+from extraction_utils.config import *
+
+def printSHAP(shap_values):
+    print("HELLO")
+    numFeatures = shap_values.shape[1]
--- lgndbdt/raw_to_bdt/BDT_Vis.py
+++ lgndbdt/raw_to_bdt/BDT_Vis.py
@@ -80,111 +80,14 @@ def run_BDT():
     sigRaw, sigDict, sigWFD, sigavse, selectDict = getRaw(filename, f"{fpath}DEP/")
     bkgRaw, bkgDict, bkgWFD, bkgavse, selectDict = getRaw(filename, f"{fpath}FEP/")
 
-    ###################################################################
-    # DATA MATCHING
-    ###################################################################
-    print("--------------- Running Distribution Matching ---------------")
-    print("-------------------------------------------------------------")
 
     sigSave, sigPDM = dataSplit(sigRaw, 0.3)
     bkgSave, bkgPDM = dataSplit(bkgRaw, 0.3)
-
-    print(f"Signal Size before Distribution Matching {sigSave.shape}")
-    for i in range(len(distMatch)):
-        sigSave, bkgSave = match_data(sigSave, bkgSave, selectDict, distMatch[i], distStep[i], True, show = False)
-    # sigSave, bkgSave = match_data(sigSave, bkgSave, selectDict, "/tdrift", 30, True, show = False)
-    # sigSave, bkgSave = match_data(sigSave, bkgSave, selectDict, "/tdrift10", 20, True, show = False)
-    # sigSave, bkgSave = match_data(sigSave, bkgSave, selectDict, "/tdrift50", 20, True, show = False)
-    # sigSave, bkgSave = match_data(sigSave, bkgSave, selectDict, "/noise", 0.00002, True, show = False)
-    # sigSave, bkgSave = match_data(sigSave, bkgSave, selectDict, "/noiseTail", 0.00002, True, show = False)
-    # sigSave, bkgSave = match_data(sigSave, bkgSave, selectDict, "/LQ80", 10, True, show = False)
-    print(f"Signal Size after Distribution Matching {sigSave.shape}")
-
-    sigs = sigSave
-    bkgs = bkgSave
-
     ###################################################################
     # TRAINING PREPARATION
     ###################################################################
-    print("----------------------- Training Prep -----------------------")
-    print("-------------------------------------------------------------")
-
-    signalTrain, signalTest = dataSplit(sigs, 0.3)
-    sigLabelTrain = np.ones(signalTrain.shape[0]) # Labels all training signals as signals (1)
-    sigLabelTest = np.ones(signalTest.shape[0]) # Labels all testing signals as signals (1)
-
-    bkgTrain, bkgTest = dataSplit(bkgs, 0.3)  # assigns arrays corresponding to randomly split signal data 
-    bkgLabelTrain = np.zeros(bkgTrain.shape[0])
-    bkgLabelTest = np.zeros(bkgTest.shape[0])
-
-    # Combining then randomizing signal and background data for training
-
-    xTrain = np.concatenate([signalTrain, bkgTrain], axis = 0) # combines input data
-    yTrain = np.concatenate([sigLabelTrain, bkgLabelTrain], axis = 0) # combines label data
-
-    trainIndex = np.arange(len(xTrain)) # Array assures same shuffling between input and label data
-    np.random.shuffle(trainIndex)
-
-    xTrain = xTrain[trainIndex]
-    yTrain = yTrain[trainIndex]
-
-    xTestStraight = np.concatenate([signalTest, bkgTest], axis = 0) # combines test input - no need to shuffle as it is for testing only
-    yTestStraight = np.concatenate([sigLabelTest, bkgLabelTest], axis = 0) # combines test label
-
-    sigTest = np.where(yTestStraight == 1)[0] # Array of signal indeces
-    backgTest = np.where(yTestStraight == 0)[0]
-
-    minTestEntry = min(len(sigTest), len(backgTest)) # Takes lesser beween num of sig and num of bkg
-
-    indexTest = list(np.random.choice(sigTest, minTestEntry, replace=False)) + list(np.random.choice(backgTest, minTestEntry, replace=False)) # list of random pull from sigTest, size of minEntryTest, list of random pull from background
-    #np.random.shuffle(indexTest) # Randomizes the order of the test datasets 
-    # This should do the same as the randomization cell but double check
-    xTestShuffle = xTestStraight[indexTest]
-    yTestShuffle = yTestStraight[indexTest]
-
-    if len(xTestShuffle)%2==1: # If len of array is odd
-        xTestShuffle = xTestShuffle[:-1] # only index to 2nd to last (making even)
-        yTestShuffle = yTestShuffle[:-1]
-        
-    xVal, xTest = np.split(xTestShuffle, 2) # splits array evenly in 2 between validation and test set
-    yVal, yTest = np.split(yTestShuffle, 2)
-
-    ###################################################################
-    # TRAINING
-    ###################################################################
-    print("------------------------- Training --------------------------")
-    print("-------------------------------------------------------------")
-
-    lgbTrain = lgb.Dataset(xTrain, yTrain, free_raw_data=False, feature_name = list(fname))
-    lgbEval = lgb.Dataset(xVal, yVal, reference=lgbTrain, free_raw_data=False, feature_name = list(fname))
-
-    # Defines the hyperparameters of the BDT 
-    params={"num_iterations": 2000, "learning_rate": learning_rate,
-            "num_leaves": num_leaves, "bagging_freq": 62, "min_data_in_leaf": 26,
-            "drop_rate": 0.3299436689754462, "min_gain_to_split": 0.5355479139953352,
-            "max_bin": max_bin, "boosting": "goss", "objective": "binary", "metric": "binary_logloss", "verbose": -100}
-    evals_result = {}
-
-    # Performs the training on the dataset
-    gbm = lgb.train(params, 
-                    lgbTrain,
-                    feature_name=list(fname), 
-                    valid_sets=lgbEval,
-                    early_stopping_rounds=10,
-                    evals_result=evals_result,
-                    verbose_eval = 20) 
-
-    explainer = shap.TreeExplainer(gbm)
-    gbm.save_model('BDT_unblind.txt') # Saves the BDT model as txt file
-
     gbm = lgb.Booster(model_file='BDT_unblind.txt')  # init model
-    TrainingMetric(evals_result) #####################
-
-    ###################################################################
-    # TREE VISUALIZATION
-    ###################################################################
-    TreeVis(gbm)#################
-
+    
     ###################################################################
     # EVALUATION AND VISUALIZATION
     ###################################################################
@@ -193,7 +96,7 @@ def run_BDT():
 
     ######################################
 
-    for i in tqdm(range(10), 
+    for i in tqdm(range(6), 
                     desc   ="Running Visualization................", 
                     colour = terminalCMAP[1]):
         if i == 0:
@@ -224,9 +127,9 @@ def run_BDT():
             np.random.shuffle(Neg_sample)
 
             sample = np.concatenate([Pos_sample[:10000], Neg_sample[:10000]],axis=0)
-            shap_values = explainer.shap_values(sample)
+            shap_valuesFull = explainer.shap_values(sample)
             # Returns a list of matrices (# outputs, # samples x, # features)
-            BDTSummary(shap_values, sample)
+            BDTSummary(shap_valuesFull, sample)
 
         elif i == 2:
             # Covariance Matrices
@@ -262,28 +165,12 @@ def run_BDT():
             plot_covariance(covBKG, "Background Covariance", covName)
 
         elif i == 4:
-            # sigsave = sigRaw
-            # bkgsave = bkgRaw
-
-            # bdt_thresh = 0.55
-            # avse_thresh = 969 #-1 # How to set Cut
-            # explainer = shap.TreeExplainer(gbm)
-
-            # sample_selector1 = (y_pred>bdt_thresh) & (Y_test == 1) & (X_test[:,selectDict["/AvsE_c"]]<avse_thresh)# & cselector
-            # sample_selector2 = (y_pred<bdt_thresh) & (Y_test == 0) & (X_test[:,selectDict["/AvsE_c"]]>avse_thresh)# & cselector
             sample_selector = sample_sig|sample_bkg
             evnew = X_test[sample_selector,:len(fname)]
             np.random.shuffle(evnew)
             evnew = evnew[:10000]
-            shap_valuesDist = explainer.shap_values(evnew)
-            make_dist_plot(evnew,shap_valuesDist[1],selectDict, "/tdrift10", "/AvsE_c")
-        elif i == 5:
-            make_dist_plot(evnew,shap_valuesDist[1],selectDict, "/tdrift", "/AvsE_c")
-        elif i == 6:
-            make_dist_plot(evnew,shap_valuesDist[1],selectDict, "/tdrift50", "/AvsE_c"),
-        elif i == 7:
-            make_dist_plot(evnew,shap_valuesDist[1],selectDict, "/tdrift", "/AvsE_c", point=True),
-        elif i == 8:
+            # shap_valuesDist = explainer.shap_values(evnew)
+            # make_dist_plot(evnew,shap_valuesDist[1],selectDict, "/tdrift10", "/AvsE_c")
             index = 0
             ROIdata = evnew #X_test[sample_selector]
 
@@ -291,9 +178,9 @@ def run_BDT():
             sample=ROIdata[index,:len(fname)].reshape(1,-1)
             shap_values = explainer.shap_values(sample)
             plot_SHAP_force(explainer, shap_values[1][0])
-        elif i == 9:
+        elif i == 5:
             plot_ROC(sigavse, bkgavse, Y_test, y_pred, sigRaw, bkgRaw, selectDict)
-    return shap_sigArr, shap_bkgArr
+    return shap_valuesFull
 
 if __name__ == "__main__":
     run_BDT()
