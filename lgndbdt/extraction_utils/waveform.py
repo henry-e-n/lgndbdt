129518f32b4ca110af59b95f0a690a59516aa46f
henry-e-n <nelliot@live.unc.edu>
2023-01-21 02:40:10 -0500

    fixed times shape

--- lgndbdt/extraction_utils/waveform.py
+++ lgndbdt/extraction_utils/waveform.py
@@ -7,12 +7,12 @@ from tqdm import tqdm
 
 def cTimes(wfParams, number, save=True):
     [dt, t0, val] = wfParams
-    t=np.zeros(val.shape)
     if number == -1:
         rg = 1 # Save small file of one row
+        t=np.zeros((1,val.shape[1]))
     else:
         rg = val.shape[0] # Save full file
-
+        t=np.zeros(val.shape)
     for event in tqdm(range(rg), desc=f"Loading Raw Data.............:", ascii=False, ncols=75, colour = terminalCMAP[0]):#):
         t[event, 0] = t0[event]
         if event > 1 and t0[event] == t0[event-1] and dt[event] == dt[event-1]:
@@ -23,7 +23,6 @@ def cTimes(wfParams, number, save=True):
             
     if save:
         np.save(f"{savePath}/timesArr_{detName}.npy", t)
-    print(t.shape)
     return t
 
 def plotWF(times, values):
