c640009387fb24458540aa3ff472e0cc29665b94
henry-e-n <nelliot@live.unc.edu>
2023-01-20 22:54:47 -0500

    tweaked init

--- __init__.py
+++ __init__.py
@@ -6,19 +6,20 @@ for path in module_path:
     if path not in sys.path:
         sys.path.append(path)
 
-print("Welcome to lgndbdt ...")
-print("This package was created by Henry Nachman for use by the LEGEND Collaboration")
-from setup import VERSION
-print(f"Current version is v{VERSION}")
+if __name__ == "__main__":
+    print("Welcome to lgndbdt ...")
+    print("This package was created by Henry Nachman for use by the LEGEND Collaboration")
+    from setup import VERSION
+    print(f"Current version is v{VERSION}")
 
-print("""Summary__________
-This package is designed to train and validate a 
-multi-site background discrimination, interpretable Boosted 
-Decision Tree Model from raw HPGe data using pulse shape parameters.
+    print("""Summary__________
+    This package is designed to train and validate a 
+    multi-site background discrimination, interpretable Boosted 
+    Decision Tree Model from raw HPGe data using pulse shape parameters.
 
-The pipeline consists of 4 major components:
-1. Energy Calibration
-2. Waveform Extraction
-3. Pulse Shape Parameter Extraction
-4. BDT Training and Validation """)
+    The pipeline consists of 4 major components:
+    1. Energy Calibration
+    2. Waveform Extraction
+    3. Pulse Shape Parameter Extraction
+    4. BDT Training and Validation """)
 # Adds the package directory to the current path so that submodules can be appropriately added
