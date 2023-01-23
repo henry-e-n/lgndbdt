1022dd76b6028bfad24a523941b3e26e5f7cff20
henry-e-n <nelliot@live.unc.edu>
2023-01-23 12:32:13 -0500

    dictionary fix

--- lgndbdt/raw_to_bdt/BDT.py
+++ lgndbdt/raw_to_bdt/BDT.py
@@ -259,8 +259,8 @@ def run_BDT(bdt_thresh = 0.55, avse_thresh = 969, SEPorFEP="SEP", plots=False):
             elif i == 4:
                 result = list(filter(lambda x: "A_" in x, selectDict))
                 print(result)
-                sigavse = sigRaw[result[0]]
-                bkgavse = bkgRaw[result[0]]
+                sigavse = sigRaw[selectDict[result[0]]]
+                bkgavse = bkgRaw[selectDict[result[0]]]
                 plot_ROC(sigavse, bkgavse, Y_test, y_pred, sigRaw, bkgRaw, selectDict, inc_ext=np.any(np.isin("/AvsE_c", fname)))
     return
 
