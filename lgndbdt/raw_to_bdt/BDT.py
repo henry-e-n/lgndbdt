aa50481537e0236a847f1697f427dfc33f12a60d
henry-e-n <nelliot@live.unc.edu>
2023-01-23 12:56:21 -0500

    made non maps .upper

--- lgndbdt/raw_to_bdt/BDT.py
+++ lgndbdt/raw_to_bdt/BDT.py
@@ -67,11 +67,11 @@ def run_BDT(bdt_thresh = 0.55, avse_thresh = 969, SEPorFEP="SEP", plots=False):
         wfd = np.zeros(2, dtype = object)
         avse = np.zeros(paramArr[0].shape[0])
         for i in range(len(paramArr)):
-            print(map(str.upper, paramArr[i].name))
-            if np.any(np.isin(map(str.upper, fname), map(str.upper, paramArr[i].name))):
+            print(paramArr[i].name.upper)
+            if np.any(np.isin(map(str.upper, fname), paramArr[i].name.upper)):
                 dataDict.append([paramArr[i].name, paramArr[i][:]])
                 dataArr[counter, :] = paramArr[i]
-                select.append([map(str.upper, paramArr[i].name), counter])
+                select.append([paramArr[i].name.upper, counter])
                 counter += 1
             if np.any(np.isin("/times", map(str.lower, paramArr[i].name))):
                 wfd[0] = paramArr[i]
