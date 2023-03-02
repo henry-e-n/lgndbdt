fbf7479b5f0b3525c54e29151650cfd762b6f5fe
henry-e-n <nelliot@live.unc.edu>
2023-03-01 23:09:47 -0500

    boxplot implementation

--- lgndbdt/ML_utils/Visualization.py
+++ lgndbdt/ML_utils/Visualization.py
@@ -471,4 +471,13 @@ def sourceLoc_distCheck(sigRAWTop, bkgRAWTop, sigRAWSide, bkgRAWSide, selectDict
     plt.hist(bkgRAWTop[:, selectDict[f"{parameter_name}"]], bins=20, histtype="step", linewidth = 3, color = terminalCMAP[1], label=f"MS Top")
     plt.hist(sigRAWSide[:, selectDict[f"{parameter_name}"]], bins=20, histtype="step", linewidth = 3, color = terminalCMAP[2], label=f"SS Side")
     plt.hist(bkgRAWSide[:, selectDict[f"{parameter_name}"]], bins=20, histtype="step", linewidth = 3, color = terminalCMAP[3], label=f"MS Side")
+    plt.legend()
     plt.savefig(f"{plotPath}/{parameter_name}DistributionHistogram.png",dpi=300, transparent=True)
+
+    
+    plt.boxplot(sigRAWTop[:, selectDict[f"{parameter_name}"]], color = terminalCMAP[0], label=f"SS Top")
+    plt.boxplot(bkgRAWTop[:, selectDict[f"{parameter_name}"]], color = terminalCMAP[1], label=f"MS Top")
+    plt.boxplot(sigRAWSide[:, selectDict[f"{parameter_name}"]], color = terminalCMAP[2], label=f"SS Side")
+    plt.boxplot(bkgRAWSide[:, selectDict[f"{parameter_name}"]], color = terminalCMAP[3], label=f"MS Side")
+    plt.legend()
+    plt.savefig(f"{plotPath}/{parameter_name}DistributionBoxPlot.png",dpi=300, transparent=True)
