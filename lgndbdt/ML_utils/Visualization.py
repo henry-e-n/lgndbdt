518a128e28f4cc7b023f79c26976e4c4a4bf0e9c
henry-e-n <nelliot@live.unc.edu>
2023-03-02 12:36:27 -0500

    lin fit tbsht

--- lgndbdt/ML_utils/Visualization.py
+++ lgndbdt/ML_utils/Visualization.py
@@ -82,11 +82,12 @@ def make_dist_plot(data, shap, selectDict, var1, var2, point=False):
 
     # selector = (data[:,index1] > xlowfit) & (data[:,index2]>-10) & (data[:,index2]<1000)
     z = np.polyfit(data[:,index1], data[:,index2],deg=1)
+    print(z)
     x = np.linspace(xlow, xhi, 10000)
     y = x * z[0] + z[1]
     # Plot
-    ymin = np.mean(data[:, index2]) - 2*np.std(data[:, index2])
-    ymax = np.mean(data[:, index2]) + 2*np.std(data[:, index2])
+    ymin = np.mean(data[:, index2]) - 4*np.std(data[:, index2])
+    ymax = np.mean(data[:, index2]) + 4*np.std(data[:, index2])
     if point == True:
         selection = (data[:, index1] < 600)
         selection = np.argwhere(selection)
