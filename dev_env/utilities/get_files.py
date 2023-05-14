bede37f496f17709c1894a52f278df56aaf1553e
henry-e-n <nelliot@live.unc.edu>
2023-05-14 17:00:30 -0400

    get files changes

--- dev_env/utilities/get_files.py
+++ dev_env/utilities/get_files.py
@@ -4,7 +4,7 @@ import os
 def get_dsp_files(detector_name, source_location):
     f = open(f"{os.getcwd()}/paths.json")
     data = json.load(f)
-    data = data["runs"][detector_name][source_location]
+    data = data[detector_name][source_location]
 
     dsp_data_dir = f"{data['path_to_dsp']}{data['detector_name']}/"
     raw_data_dir = f"{data['path_to_raw']}{data['detector_name']}/"
@@ -18,10 +18,16 @@ def get_dsp_files(detector_name, source_location):
         raw_file = raw_data_dir +  run + '.lh5'
         dsp_files.append(dsp_file)
         raw_files.append(raw_file)
-        
+    
+    file_save_path = data["file_save_path"]
     return dsp_files, raw_file, file_save_path
 
 def get_save_paths(detector_name, source_location):
-
+    f = open(f"{os.getcwd()}/paths.json")
+    data = json.load(f)
+    data = data[detector_name][source_location]
+    
+    file_save_path = data["file_save_path"]
+    plot_save_path = data["plot_save_path"]
 
     return file_save_path, plot_save_path
