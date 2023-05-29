713fa2439321d9f3166f698d4789e24e2795a895
henry-e-n <nelliot@live.unc.edu>
2023-05-29 05:40:25 -0400

    explainer in BDT test

--- lgndbdt/dev_env/run_files/BDT_test.py
+++ lgndbdt/dev_env/run_files/BDT_test.py
@@ -31,7 +31,10 @@ def BDT_test(detector_name, source_location, train_features, data_to_test, BDTmo
 
 
     MSBDT     = lgb.Booster(model_file=BDTmodel)
-    
+    MSBDTstr  = MSBDT.model_to_string()                
+    explainer = shap.TreeExplainer(MSBDT.model_from_string(MSBDTstr))
+                
+
     y_pred = MSBDT.predict(data_to_test[:,:len(train_features)], num_iteration=MSBDT.best_iteration)
     np.save("Y_pred.npy", y_pred)
 
@@ -41,4 +44,16 @@ def BDT_test(detector_name, source_location, train_features, data_to_test, BDTmo
     plt.cla()
     plt.clf()
     plt.close()
+
+    Pos_sample = data_to_test[:,:len(train_features)]
+    np.random.shuffle(Pos_sample)
+
+    sample = np.concatenate([Pos_sample],axis=0)
+    shap_values = explainer.shap_values(sample)
+    # Returns a list of matrices (# outputs, # samples x, # features)
+    BDTSummary(shap_values, sample, train_features)
+    plt.title(f"BDT SHAP Feature Importance ({source_location})")
+    plt.savefig(f"{plot_save_path}/{source_location}/bdt_summary.pdf",dpi=300, bbox_inches = 'tight', pad_inches = 0.3, transparent=True)
+
+
     return
