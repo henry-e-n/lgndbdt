dfa68e539bb9018cdfe2f497ec07090e4eecbd90
henry-e-n <nelliot@live.unc.edu>
2023-05-28 23:30:46 -0400

    changed v number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from subprocess import check_call
 
 HERE         = pathlib.Path(__file__).parent
 PACKAGE_NAME = 'lgndbdt'
-VERSION      = '0.9.9.4.4'
+VERSION      = '0.9.9.5.0'
 AUTHOR       = 'Henry Nachman'
 AUTHOR_EMAIL = 'henachman@unc.edu'
 URL          = 'https://github.com/henry-e-n/lgndbdt.git'
